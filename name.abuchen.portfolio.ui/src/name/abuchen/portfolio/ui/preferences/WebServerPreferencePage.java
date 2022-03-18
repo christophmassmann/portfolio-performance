80edeab6e63b1d07822720b46cf3b03f94ac5a89
Christoph Massmann <chris@dev-investor.de>
2022-03-18 10:41:35 +0100

    made branch compatible with latest master version

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/preferences/WebServerPreferencePage.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/preferences/WebServerPreferencePage.java
@@ -13,20 +13,20 @@ public class WebServerPreferencePage extends FieldEditorPreferencePage
     public WebServerPreferencePage()
     {
         super(GRID);
-        setTitle("Web Server [Experimental]");
+        setTitle("Web Server [Experimental]"); //$NON-NLS-1$
     }
 
     @Override
     public void createFieldEditors()
     {
         addField(new BooleanFieldEditor(UIConstants.Preferences.RUN_WEB_SERVER, //
-                        "Start embedded web server", getFieldEditorParent()));
+                        "Start embedded web server", getFieldEditorParent())); //$NON-NLS-1$
 
         addField(new IntegerFieldEditor(UIConstants.Preferences.WEB_SERVER_PORT, //
-                        "Web server port", getFieldEditorParent()));
+                        "Web server port", getFieldEditorParent())); //$NON-NLS-1$
 
         addField(new StringFieldEditor(UIConstants.Preferences.WEB_SERVER_TOKEN, //
-                        "Access token", getFieldEditorParent()));
+                        "Access token", getFieldEditorParent())); //$NON-NLS-1$
 
     }
 }
